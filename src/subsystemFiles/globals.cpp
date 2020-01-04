a08a4c2e288e6fe36e06efd3abeeebc196737a7f
Pipiate <vuong.dbrandon@gmail.com>
2020-01-04 14:19:21 -0500

    Updated code from November Comp

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -2,19 +2,53 @@
 
 //Verify gear set, maybe change ports
 pros::Motor driveLeft(20, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor driveRight(11, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor liftRight(12, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor liftLeft(19, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intakeRight(13, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRight(11, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor liftRight(13, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor liftLeft(17, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor intakeRight(12, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
 pros::Motor intakeLeft(18, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor tiltTable(17, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor tiltTable(16, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
 
+//Controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
 
+//Driver Functions
 void dumpCubes() {
-  int abutton = (controller.get_digital(pros::E_CONTROLLER_DIGITAL_A));
-  if (abutton == 1) {
-      setdrive(-25, -25);
-      setIntake(30);
+  int buttonA = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
+  if (buttonA == 1) {
+    setDrive(-25, -25);
+    setIntake(30);
   }
 }
+
+//auton helpers
+
+
+//autonomous
+void extendChute(int position){
+  pros::lcd::print(3, "tilt angle %f", tiltTable.get_position());
+  if(tiltTable.get_position() < position){
+      setTilt(62);
+  }
+  else if(tiltTable.get_position() > position){
+    setTilt(0);
+    setIntake(127);
+    pros::delay(200);
+
+      }
+
+  else
+  {
+    setTilt(0);
+  }
+}
+
+
+//misc
+float giveangle(){
+  float position = tiltTable.get_position();
+  pros::lcd::print(2, "position %f", position);
+  return position;
+}
+
+const float INCHESTOTICKS = 1800/(4*3.14159);
