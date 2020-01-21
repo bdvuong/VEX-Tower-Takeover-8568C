e6fc91d804575e7854f3382996152bc99ecfa894
vuongdbrandon <57996169+vuongdbrandon@users.noreply.github.com>
2020-01-21 10:14:09 -0500

    Update lift.cpp
    fixed to call the correct function

--- src/subsystemFiles/lift.cpp
+++ src/subsystemFiles/lift.cpp
@@ -12,7 +12,7 @@ void setLiftMotors() {
   // lower is R2, want it to out outtake
   // Upper is R1, want it to intake
   int liftPower = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2));
-  setIntake(liftPower);
+  setLift(liftPower);
 }
 
 
