e7b4c0a727dfb4c8c65f51da76731d03d2c3dbab
vuongdbrandon <vuong.dbrandon@gmail.com>
2020-01-25 01:32:42 -0500

    got rid of extraneous brackets

--- src/subsystemFiles/lift.cpp
+++ src/subsystemFiles/lift.cpp
@@ -104,5 +104,4 @@ void setLiftMotors() {
     // Upper is R1, want it to intake
     int liftPower = 127 * (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1) - controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2));
     setLiftMotors(liftPower, liftPower);
-      }
-    }
+}
