3a2b9826ebb14143af08f32bf52e8b167332e109
vuongdbrandon <vuong.dbrandon@gmail.com>
2020-01-25 01:31:55 -0500

    added more convesion factors

--- include/subsystemHeaders/globals.hpp
+++ include/subsystemHeaders/globals.hpp
@@ -28,5 +28,7 @@ extern const float INCHESTOTICKS;
 #define PI 3.14159
 #define TICKS_PER_REV_TORQUE 1800
 #define DEG_CIRCLE 360.0
-#define TICKS_DEG_TORQUE 360.0 / 1800.0;
-#define DEG_TO_TICKS 1800.0 / 360.0;
+// #define TICKS_DEG_TORQUE 360.0 / 1800.0;
+// #define DEG_TO_TICKS 1800.0 / 360.0;
+#define WHEEL_DIAM 4.0
+#define INCHES_TO_TICKS 1800.0 / (WHEEL_DIAM*PI)
