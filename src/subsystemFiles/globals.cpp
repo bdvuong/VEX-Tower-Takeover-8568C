f9a02470b57e7e0c8c6b38c76af3ce260698d5b9
vuongdbrandon <vuong.dbrandon@gmail.com>
2020-01-24 23:51:15 -0500

    adjusted port numbers, added documentation

--- src/subsystemFiles/globals.cpp
+++ src/subsystemFiles/globals.cpp
@@ -1,13 +1,13 @@
 #include "main.h"
 
 //Verify gear set, maybe change ports
-pros::Motor driveLeft(20, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor driveRight(11, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor liftRight(13, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor liftLeft(17, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intakeRight(12, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor intakeLeft(18, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor tiltTable(16, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor driveRight(10, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor driveLeft(9, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor liftRight(1, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor liftLeft(2, pros::E_MOTOR_GEARSET_36, true, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
+pros::Motor intakeRight(3, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_COUNTS); //standard gear ratio
+pros::Motor intakeLeft(4, pros::E_MOTOR_GEARSET_18, true, pros::E_MOTOR_ENCODER_COUNTS); //standard gear ratio
+pros::Motor tiltTable(8, pros::E_MOTOR_GEARSET_36, false, pros::E_MOTOR_ENCODER_COUNTS); //torque motors
 
 //Controller
 pros::Controller controller(pros::E_CONTROLLER_MASTER);
@@ -18,6 +18,8 @@ pros::ADIEncoder trackingRight('C', 'D');
 pros::ADIEncoder trackingLeft('G', 'H');
 
 //driver macros
+
+//this macro makes the bot move backwards slowly and pushes the stack outwards
 void dumpCubes() {
   int buttonA = controller.get_digital(pros::E_CONTROLLER_DIGITAL_A);
   if (buttonA == 1) {
@@ -27,6 +29,7 @@ void dumpCubes() {
 }
 
 //misc
+
 float giveTiltPosition(){
   float position = tiltTable.get_position();
   pros::lcd::print(2, "position %f", position);
