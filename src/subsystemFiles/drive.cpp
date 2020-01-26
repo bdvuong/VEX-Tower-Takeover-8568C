3521143ea8d13cdb3cc1b032dc4a7138eca0a8ae
vuongdbrandon <vuong.dbrandon@gmail.com>
2020-01-26 13:49:12 -0500

    changed movement to one joystick instead of one to move one to turn

--- src/subsystemFiles/drive.cpp
+++ src/subsystemFiles/drive.cpp
@@ -34,14 +34,14 @@ double avgDriveEncoderValue() {
 
 //ARCADE CONTROL
 void setDriveMotors() {
-  int leftJoystick = (controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
-  int rightJoystick = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
-  if(abs(leftJoystick) < 10)
-    leftJoystick = 0;
-  if(abs(rightJoystick) < 10)
-    rightJoystick = 0;
-  int leftPower = leftJoystick + rightJoystick;
-  int rightPower =  leftJoystick - rightJoystick;
+  int yAxis = (controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y));
+  int xAxis = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_X);
+  if(abs(yAxis) < 10)
+    yAxis = 0;
+  if(abs(xAxis) < 10)
+    xAxis = 0;
+  int leftPower = yAxis + xAxis;
+  int rightPower =  yAxis - xAxis;
   setDrive(leftPower, rightPower);
 }
 
